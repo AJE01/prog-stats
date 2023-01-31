e2d49c414c63493dc350f2e32e5c8f9d47be6846
AJE01 <estevez5@go.stockton.edu>
2023-01-31 17:28:32 -0500

    Committed

---
+++ progstats/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author hoonglops
+ *
+ */
+module progstats {
+}
---
+++ progstats/src/progstats/testGitHub.java
@@ -0,0 +1,10 @@
+package progstats;
+
+public class testGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
